6d296e19166ce017d1cf4f4dab7185085ff5e58a
Виталий Кеков <hauhai322@gmail.com>
2023-09-05 14:38:54 +0300

    first try

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte(`https://practicum.yandex.ru/`))
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain")
+	_, _ = w.Write([]byte(`http://localhost:8080/EwHXdJfB `))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, GetHandler)
+	mux.HandleFunc(`/EwHXdJfB`, mainPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
