d07a3ee5fdc818239b11c82c0f842ad96966ea38
Виталий Кеков <hauhai322@gmail.com>
2023-09-06 09:19:10 +0300

    try2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,13 +4,38 @@ import (
 	"net/http"
 )
 
+var urlMap = make(map[string]string)
+
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
 		return
 	}
+
+	// Чтение URL из тела POST-запроса.
+	urlBytes := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(urlBytes)
+	if err != nil {
+		http.Error(w, "Failed to read URL from request body", http.StatusInternalServerError)
+		return
+	}
+	url := string(urlBytes)
+
+	// Генерация уникального идентификатора (короткого пути) и сохранение URL.
+	id := generateID()
+	urlMap[id] = url
+
+	// Отправка ответа с коротким URL.
 	w.Header().Set("Content-Type", "text/plain")
-	_, _ = w.Write([]byte(`https://practicum.yandex.ru/`))
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(id))
+}
+
+func generateID() string {
+	// Здесь может быть логика для генерации уникального ID,
+	// например, с использованием случайной генерации или хеширования.
+	// В данном примере, используется фиксированный ID.
+	return "EwHXdJfB"
 }
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,14 +43,26 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
 		return
 	}
-	w.Header().Set("Content-Type", "text/plain")
-	_, _ = w.Write([]byte(`http://localhost:8080/EwHXdJfB `))
+
+	// Извлечение идентификатора из URL.
+	id := r.URL.Path[1:] // Удаление первого символа "/", чтобы получить идентификатор.
+
+	// Поиск URL по идентификатору в карту urlMap.
+	url, exists := urlMap[id]
+	if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Отправка ответа с перенаправлением и оригинальным URL в заголовке Location.
+	w.Header().Set("Location", url)
+	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, GetHandler)
-	mux.HandleFunc(`/EwHXdJfB`, mainPage)
+	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/EwHXdJfB`, GetHandler)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
