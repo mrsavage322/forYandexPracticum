afe9fb4260ae44e9c37486553a7e94ef1bccf8e0
Savage <snayperlvov1@gmail.com>
2023-10-17 16:09:31 +0300

    refactor interface

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,8 +1,25 @@
 package storage
 
 type URLStorage interface {
+	SetURL
+	GetURL
+}
+
+type GetURL interface {
 	Get(key string) (string, bool)
+}
+
+func (s *URLMapStorage) Get(key string) (string, bool) {
+	value, ok := s.data[key]
+	return value, ok
+}
+
+type SetURL interface {
 	Set(key, value string)
+}
+
+func (s *URLMapStorage) Set(key, value string) {
+	s.data[key] = value
 }
 
 type URLMapStorage struct {
@@ -13,13 +30,4 @@ func NewURLMapStorage() URLStorage {
 	return &URLMapStorage{
 		data: make(map[string]string),
 	}
-}
-
-func (s *URLMapStorage) Get(key string) (string, bool) {
-	value, ok := s.data[key]
-	return value, ok
-}
-
-func (s *URLMapStorage) Set(key, value string) {
-	s.data[key] = value
 }
