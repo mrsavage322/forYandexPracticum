9f1325383b28362bcec3d9045c894395c21109fa
Savage <snayperlvov1@gmail.com>
2023-10-29 19:23:50 +0300

    minor fix

--- internal/app/handlejson.go
+++ internal/app/handlejson.go
@@ -4,15 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	_ "reflect"
-	_ "strings"
-	_ "time"
 )
-
-//const (
-//	targetField = "User" // имя поля, о котором нужно получить информацию
-//	targetTag   = "json" // тег, значение которого нужно получить
-//)
 
 type Request struct {
 	URL string `json:"url"`
@@ -21,10 +13,6 @@ type Request struct {
 type Response struct {
 	Result string `json:"result"`
 }
-
-//type JSONResponse struct {
-//	Result string `json:"result"`
-//}
 
 func HandleJSON(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
@@ -35,27 +23,7 @@ func HandleJSON(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//var jsonResponse JSONResponse
-	//err = json.Unmarshal([]byte(req.Result), &jsonResponse)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusBadRequest)
-	//	return
-	//}
-
 	link := req.URL
-
-	//obj := Response{}
-	//objType := reflect.TypeOf(req)
-	//field, ok := objType.FieldByName(targetField)
-	//if !ok {
-	//	panic(fmt.Errorf("field (%s): not found", targetField))
-	//}
-	//tagValue, ok := field.Tag.Lookup(targetTag)
-	//if !ok {
-	//	panic(fmt.Errorf("tag (%s) for field (%s): not found", targetTag, targetField))
-	//}
-
-	//link := strings.Split(tagValue, ",")
 	id := GenerateRandomID(5)
 	shortURL := fmt.Sprintf("%s/%s", BaseURL, id)
 	URLMap.Set(id, link)
