1a792c0e71630fa48b330aab21073ca7844c0250
Savage <snayperlvov1@gmail.com>
2023-10-29 14:27:18 +0300

    fix logger logic

--- internal/app/logger.go
+++ internal/app/logger.go
@@ -1,91 +1,77 @@
 package app
 
 import (
-	"bytes"
 	"go.uber.org/zap"
-	"io"
 	"net/http"
 	"time"
 )
 
-// var sugarLogger *zap.SugaredLogger
-// var logger *zap.Logger
-var requestLogger *zap.SugaredLogger
-var responseLogger *zap.SugaredLogger
+var sugar zap.SugaredLogger
+
+type (
+	responseData struct {
+		status int
+		size   int
+	}
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
 
 func InitializeLogger() {
-	var err error
 	logger, err := zap.NewDevelopment()
 	if err != nil {
-		panic("cannot initialize zap logger: " + err.Error())
+		panic("Can not initialize zap logger: " + err.Error())
 	}
 	defer logger.Sync()
 
-	requestLogger = logger.Sugar().Named("Request")
-	responseLogger = logger.Sugar().Named("Response")
+	sugar = *logger.Sugar()
 }
 
 // LogRequest логирует информацию о запросе.
-func LogRequest(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func LogRequest(h http.Handler) http.Handler {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
-		responseWriter := NewStatusSizeLoggingResponseWriter(w)
 
-		var requestBody []byte
-		if r.Body != nil {
-			requestBody, _ = io.ReadAll(r.Body)
-			r.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+		uri := r.RequestURI
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
 		}
 
-		next.ServeHTTP(w, r)
-		elapsed := time.Since(startTime)
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&lw, r) // внедряем реализацию http.ResponseWriter
 
-		requestLogger.Infow("Request",
-			"URL", r.URL,
-			"Status", responseWriter.Status(),
+		duration := time.Since(startTime)
+
+		sugar.Infow("Request",
+			"URI", uri,
 			"Method", r.Method,
-			"Size", responseWriter.Size(),
-			"RequestBody", string(requestBody),
-			"Elapsed", elapsed,
+			"Duration", duration,
 		)
 
-		responseLogger.Infow("Response",
-			"URL", r.URL, // Если необходимо логировать URL
-			"Status", http.StatusCreated,
-			"Size", len(shortURL), // Размер ответа
+		sugar.Infow("Response",
+			"Status", responseData.status,
+			"Size", responseData.size,
 		)
-	})
-}
-
-func NewStatusSizeLoggingResponseWriter(w http.ResponseWriter) *StatusSizeLoggingResponseWriter {
-	return &StatusSizeLoggingResponseWriter{
-		ResponseWriter: w,
-		status:         http.StatusOK, // По умолчанию, предполагаем успешный статус
-		size:           0,
 	}
-}
-
-type StatusSizeLoggingResponseWriter struct {
-	http.ResponseWriter
-	status int
-	size   int
-}
-
-func (w *StatusSizeLoggingResponseWriter) WriteHeader(code int) {
-	w.status = code
-	w.ResponseWriter.WriteHeader(code)
-}
-
-func (w *StatusSizeLoggingResponseWriter) Write(b []byte) (int, error) {
-	n, err := w.ResponseWriter.Write(b)
-	w.size += n
-	return n, err
-}
-
-func (w *StatusSizeLoggingResponseWriter) Status() int {
-	return w.status
-}
-
-func (w *StatusSizeLoggingResponseWriter) Size() int {
-	return w.size
+	return http.HandlerFunc(logFn)
 }
