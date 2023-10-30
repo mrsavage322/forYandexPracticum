33e2e1fe3f58b9602a3a775ed1062070580d2179
Savage <snayperlvov1@gmail.com>
2023-10-30 10:38:53 +0300

    fix gzip

--- internal/app/handlegzip.go
+++ internal/app/handlegzip.go
@@ -16,20 +16,17 @@ func (grw GzipResponseWriter) Write(b []byte) (int, error) {
 	return grw.Writer.Write(b)
 }
 
-// Функция, которая проверяет, поддерживает ли клиент сжатие.
 func isGzipSupported(r *http.Request) bool {
 	return strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
 }
 
-// Middleware для обработки сжатия.
 func GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if isGzipSupported(r) {
 			w.Header().Set("Content-Encoding", "gzip")
 			gz := gzip.NewWriter(w)
 			defer gz.Close()
-			gw := GzipResponseWriter{ResponseWriter: w, Writer: gz}
-			next.ServeHTTP(gw, r)
+			next.ServeHTTP(GzipResponseWriter{ResponseWriter: w, Writer: gz}, r)
 		} else {
 			next.ServeHTTP(w, r)
 		}
