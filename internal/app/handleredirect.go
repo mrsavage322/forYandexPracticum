4e8a7aca2df4e05b8a33c4a9a440498174d2b4f3
Savage <snayperlvov1@gmail.com>
2023-11-12 18:13:01 +0300

    minor fix redirect bd

--- internal/app/handleredirect.go
+++ internal/app/handleredirect.go
@@ -7,13 +7,22 @@ import (
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
-	originalURL, ok := URLMapDB.Get(id)
-	if ok == nil {
-		http.Error(w, "Non-existent identifier", http.StatusBadRequest)
-		return
-	}
+	if DatabaseAddr != "" {
+		originalURL, ok := URLMapDB.Get(id)
+		if ok == nil {
+			http.Error(w, "Non-existent identifier", http.StatusBadRequest)
+			return
+		}
+		w.Header().Set("Location", originalURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
 
-	// Выполняем перенаправление на оригинальный URL с кодом 307
-	w.Header().Set("Location", originalURL)
-	w.WriteHeader(http.StatusTemporaryRedirect)
+	} else {
+		originalURL, ok := URLMap.Get(id)
+		if !ok {
+			http.Error(w, "Non-existent identifier", http.StatusBadRequest)
+			return
+		}
+		w.Header().Set("Location", originalURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	}
 }
