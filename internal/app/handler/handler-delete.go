f2e35421b9499ad8e739fe75e5d5728d1d7f6cb8
Savage <snayperlvov1@gmail.com>
2023-11-29 01:22:53 +0300

    fix goroutine logic

--- internal/app/handler/handler-delete.go
+++ internal/app/handler/handler-delete.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	_ "sync"
 )
 
 func DeleteURLsHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,25 +20,32 @@ func DeleteURLsHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		resultChan := make(chan error, len(urls))
 		var wg sync.WaitGroup
+		wg.Add(len(urls))
 
-		go func() {
-			defer wg.Done()
-			for _, url := range urls {
+		for _, url := range urls {
+			go func(url string) {
+				defer wg.Done()
 				err := app.Cfg.URLMapDB.DeleteDBPrepare(url, app.Cfg.UserID)
 				resultChan <- err
 				if err != nil {
 					log.Println("Problem to remove url from BD ", err)
-					return
 				}
-			}
-		}()
+			}(url)
+		}
+
 		go func() {
 			wg.Wait()
 			close(resultChan)
 		}()
+
+		for err := range resultChan {
+			if err != nil {
+				log.Println("Error during deletion:", err)
+			}
+		}
+
 		for _, url := range urls {
 			err := app.Cfg.URLMapDB.DeleteDBFinally(url, app.Cfg.UserID)
-			resultChan <- err
 			if err != nil {
 				log.Println("Problem to remove url from BD ", err)
 				return
